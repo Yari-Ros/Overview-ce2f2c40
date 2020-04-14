2eed4f6512381d88b3843ab8e6682ed28b8d8ee9
Yari-Ros <2118088@talnet.nl>
2020-04-14 12:50:32 +0200

    Uitwerking van de exercise

--- index.php
+++ index.php
@@ -20,4 +20,22 @@ try {
 }
 echo ('connected to ' . $db  . ' using ');
 echo $pdo->query('select version()')->fetchColumn();
+
+echo "<br/> <br/> <br/>" . "Series" . "<br/>";
+
+$stmt = $pdo->query('SELECT * FROM netland.series;');
+while ($row = $stmt->fetch())
+{
+    echo "<br/>" . $row['title'] . " - Rating:  " . $row['rating'];
+}
+
+echo "<br/> <br/> <br/>" . "Movies" . "<br/>";
+
+$stmt = $pdo->query('SELECT * FROM netland.movies;');
+while ($row = $stmt->fetch())
+{
+    echo "<br/>" . $row['title'] . " - Duur:  " . $row['duur'] . "min";
+}
+
+// Mocht er toch nog een error zijn msg me dan // 
 ?>  
